43858d1bd0e7ef108135c9593716bdaca408c0cb
Moin Sekander <moinuddin8510@gmail.com>
2022-09-04 04:35:11 -0400

    implement post video endpoint to server

--- routes/videos.js
+++ routes/videos.js
@@ -23,18 +23,57 @@ router.get('/', (req, res) => {
         }
     })
 
+    if (strippedVideos.length === 0) {
+        res.status(404).json({
+            message: "No videos posted, make an upload and request again",
+        })
+    }
+
     res.json(strippedVideos);
 })
 
 router.get('/:id', (req, res) => {
     const videos = readVideos();
-    console.log((videos[0]))
 
     const foundVideo = videos.find((video) => {
         return video.id === req.params.id
     })
 
+    if (foundVideo === undefined) {
+        res.status(404).json({
+            message: "No video with that id exists"
+        })
+    }
+
     res.json(foundVideo);
+})
+
+router.post('/', (req, res) => {
+    if (!(req.body.title && req.body.description)) {
+        res.status(400).json({
+            message: "Please fill out all the required information and resubmit"
+        })
+    }
+
+    const newVideo = {
+        title: req.body.title,
+        channel: "TestUser",
+        image: "http://localhost:8080/images/Upload-video-preview.jpg",
+        description: req.body.description,
+        views: "0",
+        likes: "0",
+        duration: "11:11",
+        video: "https://project-2-api.herokuapp.com/stream",
+        timestamp: Date.now(),
+        comments: [],
+        id: crypto.randomUUID(),
+    };
+
+    const videos = readVideos();
+    videos.push(newVideo);
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+
+    res.status(201).json(newVideo);
 })
 
 
